f532b73b5fdf396b4f639c94bb163d04999ae819
ballanse <devblackholes@gmail.com>
2017-11-24 16:25:45 -0200

    search funcionando

--- src/client/containers/Produtos/Index.js
+++ src/client/containers/Produtos/Index.js
@@ -7,8 +7,11 @@ import GridLayout from '../../components/GridLayout';
 @inject('store') @observer
 export default class ProdutosIndex extends React.Component {
 
-  static fetchData({ store, api }) {
-    return api.get('http://ec2-54-207-63-160.sa-east-1.compute.amazonaws.com:3000', 'products', { group: 'grupo6' })
+  static fetchData({ store, api, location }) {
+    return api.get('http://ec2-54-207-63-160.sa-east-1.compute.amazonaws.com:3000', 'products', {
+      group: 'grupo6',
+      name: location.query.name || ''
+    })
       .then((data) => {
         store.produtos = data;
         return data;
