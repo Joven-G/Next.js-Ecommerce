f90797e87afcb9f19a2010a3c7567a47b32a3548
ballanse <devblackholes@gmail.com>
2017-11-24 15:40:25 -0200

    arrumando as categorias e carrossel

--- src/client/components/Carrossel.js
+++ src/client/components/Carrossel.js
@@ -7,25 +7,16 @@ export default class Carrossel extends React.Component {
     return (
       <Carousel>
         <Carousel.Item bsClass="carousel-promo">
-          <img className="img-carousel" alt="PROMO" src="/public/static/promocao.jpg" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          <img className="img-carousel"  alt="PROMO" src="/public/static/banner1.jpg" />
+
         </Carousel.Item>
         <Carousel.Item bsClass="carousel-promo">
-          <img className="img-carousel" alt="450x250" src="/public/static/Promocao-banner.png" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <img className="img-carousel" alt="450x250" src="/public/static/banner2.jpeg" />
+
         </Carousel.Item>
         <Carousel.Item bsClass="carousel-promo">
-          <img className="img-carousel" alt="450x250" src="/public/static/promo.jpg" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
+          <img className="img-carousel" alt="450x250" src="/public/static/banner3.jpg" />
+
         </Carousel.Item>
       </Carousel>
     );
--- src/client/components/Menu.js
+++ src/client/components/Menu.js
@@ -9,9 +9,9 @@ export default class Menu extends React.Component {
     return (
       <Button.Group id="menu" size="huge">
         <Button as={Link} to="/produtos/eletronicos">Eletrônicos</Button>
-        <Button href="#">Lingeries</Button>
-        <Button href="#">Acessórios</Button>
-        <Button href="#">Cosméticos</Button>
+        <Button as={Link} to="/produtos/moda">Moda</Button>
+        <Button as={Link} to="/produtos/moveis">Móveis</Button>
+        <Button as={Link} to="/produtos/materialescolar">Material Escolar</Button>
       </Button.Group>
     );
   }
